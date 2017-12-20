7a0ad0d94aa93288bdeab085171b24fc07217af8
Matt Sweeney <matt@savvior.com>
2017-12-20 17:10:04 -0500

    init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class RS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.PeopleLabel = new System.Windows.Forms.Label();
+            this.Display = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadBtn.Location = new System.Drawing.Point(282, 346);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoadBtn.TabIndex = 0;
+            this.LoadBtn.Text = "Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // PeopleLabel
+            // 
+            this.PeopleLabel.AutoSize = true;
+            this.PeopleLabel.Location = new System.Drawing.Point(164, 84);
+            this.PeopleLabel.Name = "PeopleLabel";
+            this.PeopleLabel.Size = new System.Drawing.Size(31, 13);
+            this.PeopleLabel.TabIndex = 2;
+            this.PeopleLabel.Text = "Skills";
+            // 
+            // Display
+            // 
+            this.Display.FormattingEnabled = true;
+            this.Display.Location = new System.Drawing.Point(12, 127);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(333, 95);
+            this.Display.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Firemaking"});
+            this.comboBox1.Location = new System.Drawing.Point(118, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // RS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 381);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.PeopleLabel);
+            this.Controls.Add(this.LoadBtn);
+            this.Name = "RS";
+            this.Text = "RS Skill";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Label PeopleLabel;
+        private System.Windows.Forms.ListBox Display;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Windows.Forms.VisualStyles;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class RS : Form
+    {
+        static HttpClient client = new HttpClient();
+
+
+
+        public RS()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            
+            Display.Items.Clear();
+            int skill = 0;
+            string index =comboBox1.SelectedItem.ToString();
+            if(index == "Firemaking")
+            {
+                skill = 13;
+            }
+            List<string> rsUsers = new List<string>();
+            rsUsers.Add("HC Dankmemer");
+            rsUsers.Add("Iron Groleau");
+            rsUsers.Add("dank_mememan");
+            string sURL;
+            foreach (string name in rsUsers) {
+                sURL = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + name;
+                WebRequest wrGETURL;
+                wrGETURL = WebRequest.Create(sURL);
+                WebProxy myProxy = new WebProxy("myproxy", 80);
+                myProxy.BypassProxyOnLocal = true;
+
+                wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+                Stream objStream;
+                objStream = wrGETURL.GetResponse().GetResponseStream();
+                StreamReader objReader = new StreamReader(objStream);
+                string sLine = "";
+                int i = 0;
+                while (sLine != null)
+                {
+                    i++;
+                    sLine = objReader.ReadLine();
+
+                    if (i == skill)
+                    {
+                        List<string> result = sLine.Split(',').ToList();
+                        Display.Items.Add(name + " has a firemaking lvl of " + result[1]);
+                    }
+                }
+                Console.ReadLine();
+
+            }
+        }
+
+        private void skillComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
